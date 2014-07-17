6d4768b5004252b679b426f6e1e846a06109f8dc
MiKKo7 <mitja_placer@yahoo.com>
2014-07-17 13:17:04 +0200

    Preden nardimo avtomatični spinner

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.*;
 import android.view.*;
 import android.widget.*;
 import android.location.*;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -28,20 +27,26 @@ import android.widget.AdapterView.OnItemClickListener;
 
 import com.mycompany.myapp.IntentIntegrator;
 import com.mycompany.myapp.IntentResult;
-
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseInstallation;
 import com.parse.PushService;
+
+import android.support.v4.app.FragmentActivity;
+
 import java.util.*;
+
 import android.provider.*;
 import android.content.Context;
 import android.content.res.Resources;
+
 import java.io.*;
 import java.text.*;
-//import java.lang.Object;
-
 import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
@@ -49,6 +54,9 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.location.Location;
 
@@ -60,13 +68,15 @@ import android.location.Location;
 
 
 
-public class MainActivity extends Activity implements OnClickListener, GooglePlayServicesClient.ConnectionCallbacks,
+public class MainActivity extends FragmentActivity implements OnClickListener, GooglePlayServicesClient.ConnectionCallbacks,
 GooglePlayServicesClient.OnConnectionFailedListener 
 {
 	
 	private Button scanBtn;
 	private Button takePicBtn;
 	private TextView formatTxt, contentTxt;
+	
+	private Button btnFind;
 	
 	private static String logtag = "barcode";
 	private Uri fileUri;
@@ -83,7 +93,23 @@ GooglePlayServicesClient.OnConnectionFailedListener
 	
 	private String storageState;
 	
+	double latitude = 0;
+	double longitude = 0;
+	
+	// Global variable to hold the current location
+    Location mCurrentLocation;
+	
 	ImageView selectedImage;  
+	
+	GoogleMap mGoogleMap;
+    Spinner mSprPlaceType;
+    Spinner mSprPlaceChoose;
+ 
+    String[] mPlaceType=null;
+    String[] mPlaceTypeName=null;
+ 
+    //double mLatitude=0;
+    //double mLongitude=0;
 	
 	//private Integer[] mImageIds = {
 		//R.drawable.image1,
@@ -105,7 +131,7 @@ GooglePlayServicesClient.OnConnectionFailedListener
 	LinearLayout imageGallery;
 	
 	// A request to connect to Location Services
-private LocationRequest mLocationRequest;
+//private LocationRequest mLocationRequest;
 
 // Stores the current instantiation of the location client in this object
 private LocationClient mLocationClient;
@@ -166,6 +192,8 @@ private LocationClient mLocationClient;
 		
 		storageState = Environment.getExternalStorageState();
 		Log.d("mycompany.myapp","Storidž:" + storageState);
+		
+		 mLocationClient = new LocationClient(this, this, this);
         
 		
 		//FancyCoverFlow.setAdapter(new ViewGroupExampleAdapter());
@@ -185,6 +213,37 @@ private LocationClient mLocationClient;
 		takePicBtn = (Button)findViewById(R.id.takePic_button);
 		
 		takePicBtn.setOnClickListener(this);
+		
+		// Tuki dodajamo!
+		
+		// Array of place types
+        mPlaceType = getResources().getStringArray(R.array.place_type);
+ 
+        // Array of place type names
+        mPlaceTypeName = getResources().getStringArray(R.array.place_type_name);
+ 
+        // Creating an array adapter with an array of Place types
+        // to populate the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, mPlaceTypeName);
+ 
+        // Getting reference to the Spinner
+        mSprPlaceType = (Spinner) findViewById(R.id.spr_place_type);
+ 
+        // Setting adapter on Spinner to set place types
+        mSprPlaceType.setAdapter(adapter);
+        
+        // Getting reference to the Choose Spinner
+        mSprPlaceChoose = (Spinner) findViewById(R.id.spr_place_choose);
+ 
+        // Setting adapter on Choose Spinner to set place
+        mSprPlaceChoose.setAdapter(adapter);
+ 
+       // Button btnFind;
+ 
+        // Getting reference to Find Button
+        btnFind = ( Button ) findViewById(R.id.btn_find);
+		
+		// Tuki nehamo dodajat!
 		
 		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
 											Environment.DIRECTORY_PICTURES), "ShopCo");									
@@ -219,7 +278,7 @@ private LocationClient mLocationClient;
 		
 		checkPlayServices();
 		
-		mLocationClient.connect(); 
+		//mLocationClient.connect(); TA TUKAJ DELA KAZIN!!!
 		
 		// clicklistener for Gallery
 		//gallery.setOnItemClickListener(new OnItemClickListener() {
@@ -476,7 +535,41 @@ private LocationClient mLocationClient;
     public void onConnected(Bundle dataBundle) {
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        mCurrentLocation = mLocationClient.getLastLocation();
+        latitude = mCurrentLocation.getLatitude();
+        longitude = mCurrentLocation.getLongitude();    
+        Log.i("Location", ""+latitude + " "+longitude);
+        
+        // Tu smo dodali!
+        
+     // Setting click event lister for the find button
+        btnFind.setOnClickListener(new OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+
+                int selectedPosition = mSprPlaceType.getSelectedItemPosition();
+                String type = mPlaceType[selectedPosition];
+
+                StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                sb.append("location="+latitude+","+longitude);
+                //sb.append("&radius=500");
+                sb.append("&types=bus_station");
+                //sb.append("&types="+type);
+                sb.append("&sensor=true");
+                sb.append("&key=AIzaSyDw084R-npWWbdq5R0JAXFZPD3lFFUocgs");
+
+                // Creating a new non-ui thread task to download json data
+                PlacesTask placesTask = new PlacesTask();
+
+                // Invokes the "doInBackground()" method of the class PlaceTask
+                placesTask.execute(sb.toString());
+
+            }
+        });
+
+     // Tu smo nehali dodajat!
+        
     }
    
     /*
@@ -528,6 +621,8 @@ private LocationClient mLocationClient;
 	@Override
 	protected void onResume() {
 		super.onResume();
+		//mLocationRequest = LocationRequest.create();
+		mLocationClient.connect(); //TA TUKAJ DELA KAZIN!!!
 //		Log.i("Location Updates",
 //			  "Smo pred Google Play services.");
 //		GooglePlayCheck cekirajmo = new GooglePlayCheck(this);
@@ -562,6 +657,17 @@ private LocationClient mLocationClient;
 //		}
 //		}
 //    
+	
+	// * Called when the Activity is no longer visible.
+    //  */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+	
+	
 	public void onClick(View v){
 
 		//respond to clicks
@@ -671,13 +777,168 @@ private LocationClient mLocationClient;
 			}
 			return false;
 		}
+		 // In debug mode, log the status
+        Log.d("Location Updates",
+                "Google Play services is available.");
+        // Continue
 		return true;
 	} 
 
+	
+	 /** A method to download json data from url */
+    private String downloadUrl(String strUrl) throws IOException{
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+ 
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+ 
+            // Connecting to url
+            urlConnection.connect();
+ 
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+ 
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+ 
+            StringBuffer sb  = new StringBuffer();
+ 
+            String line = "";
+            while( ( line = br.readLine())  != null){
+                sb.append(line);
+            }
+ 
+            data = sb.toString();
+ 
+            br.close();
+ 
+        }catch(Exception e){
+            Log.d("Exception while downloading url", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+ 
+        return data;
+    }
+	
+	
+	 /** A class, to download Google Places */
+    private class PlacesTask extends AsyncTask<String, Integer, String>{
+ 
+        String data = null;
+ 
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url) {
+            try{
+                data = downloadUrl(url[0]);
+                Log.i("Data od downloadUrl", data);
+            }catch(Exception e){
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+ 
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result){
+            ParserTask parserTask = new ParserTask();
+ 
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParseTask
+            parserTask.execute(result);
+        }
+ 
+    }
+	
+	
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String,String>>>{
+ 
+        JSONObject jObject;
+ 
+        // Invoked by execute() method of this object
+        @Override
+        protected List<HashMap<String,String>> doInBackground(String... jsonData) {
+ 
+            List<HashMap<String, String>> places = null;
+            PlaceJSONParser placeJsonParser = new PlaceJSONParser();
+ 
+            try{
+                jObject = new JSONObject(jsonData[0]);
+ 
+                /** Getting the parsed data as a List construct */
+                places = placeJsonParser.parse(jObject);
+ 
+            }catch(Exception e){
+                Log.d("Exception",e.toString());
+            }
+            return places;
+        }
+ 
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(List<HashMap<String,String>> list){
+ 
+            // Clears all the existing markers
+            //mGoogleMap.clear();
+        	Log.i("List size", Integer.toString(list.size()));
+ 
+            for(int i=0;i<list.size();i++){
+ 
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+ 
+                // Getting a place from the places list
+                HashMap<String, String> hmPlace = list.get(i);
+ 
+                // Getting latitude of the place
+                double lat = Double.parseDouble(hmPlace.get("lat"));
+ 
+                // Getting longitude of the place
+                double lng = Double.parseDouble(hmPlace.get("lng"));
+ 
+                // Getting name
+                String name = hmPlace.get("place_name");
+                Log.i("Kraj", name);
+               
+ 
+                // Getting vicinity
+                String vicinity = hmPlace.get("vicinity");
+ 
+                LatLng latLng = new LatLng(lat, lng);
+ 
+                // Setting the position for the marker
+               // markerOptions.position(latLng);
+ 
+                // Setting the title for the marker.
+                //This will be displayed on taping the marker
+              //  markerOptions.title(name + " : " + vicinity);
+ 
+                // Placing a marker on the touched position
+              //  mGoogleMap.addMarker(markerOptions);
+            }
+        }
+    }
+    
+    
+    
+	
 	void showErrorDialog(int code) {
 		GooglePlayServicesUtil.getErrorDialog(code, this, 
 											  REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
 	}
+
+
+//	@Override
+//	public void onLocationChanged(Location arg0) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 	
 	
 }
